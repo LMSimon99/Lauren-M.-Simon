0cdfaa0618efb9c9a5079d0d0aad61fe8495b985
LMSimon99 <54373647+LMSimon99@users.noreply.github.com>
2019-08-21 14:35:13 -0400

    1?

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*************************************************************************
+ * Java Program Name
+ *************************************************************************
+ * Project/Program description
+ * _______________________________________________________________________
+ * Lauren Simon
+ * 21 August 2019
+ * CMSC 255 002
+ ************************************************************************/
+ 
+ public class Hello {
+	public static void printHeading() {
+		System.out.println("Lauren");
+		System.out.println("Java Project 1");
+		System.out.println("Project 1");
+		System.out.println("21 August 2019");
+		System.out.println("CMSC 255 002");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+ }
